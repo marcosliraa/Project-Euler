aeab99e6fa7a6de256258c891f18b415a727d2e8
José Marcos de Lira Júnior <jmlj.lira@gmail.com>
2016-07-22 20:27:56 -0300

    Adicionada o arquivo

--- ex01.c
+++ ex01.c
@@ -1,1 +1,19 @@
 /* Primeiro Exercício do Project Euler*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int soma,i;
+    soma=0;
+    for(i=1;i<11;i++)
+    {
+        if((i%3==0) ||(i%5==0))
+            soma= soma + i;
+        i++;
+    }
+    
+    printf("%d\n",soma);
+    return 0;
+}
